10a7acc8e7ebf64217a95afc909ee9dced1e9607
Bayman1991 <bayman1991@gmail.com>
2018-02-02 18:25:36 +0200

    Adding frontend files to Github Pages

--- truffle.js
+++ truffle.js
@@ -3,14 +3,25 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      from: '0xA2a90d735fC6E6752de0CFCC75CC26885936DDc5'
     },
+
+    live: {
+      host: "localhost",
+      port: 8545,
+      network_id: 1,
+      gas: 4700000,
+      from: '0xA2a90d735fC6E6752de0CFCC75CC26885936DDc5'
+    },
+
     rinkeby: {
       host: "localhost",
-      port: 8454,
+      port: 8545,
       network_id: 4,
       gas: 4000000,
       from: 0x8edF467aedc47877486C4055fc484e76AE77fEDF
     }
-  }
+  },
+  solc: { optimizer: { enabled: true, runs: 200 } }
 };
